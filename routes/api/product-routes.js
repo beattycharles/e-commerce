cc1182d29f64371bee6737b2062933fa99a08b91
beattycharles <113803621+beattycharles@users.noreply.github.com>
2023-01-24 16:59:43 -0500

    corrected products route return data

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
     });
-    res.status(500).json(err);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
